659d96caff8ec519bd0e04801b5be928f18f7023
Bình Duy <166035036+binhduy1402@users.noreply.github.com>
2024-12-18 11:53:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const dotenv=require("dotenv");
-const connectDB =require('./connection');
-const app=express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require('./connection');
+const app = express();
 const cookieParser = require("cookie-parser");
-dotenv.config()
+dotenv.config();
+
 var multer = require('multer');
 
 const PORT = process.env.PORT || 8072;
@@ -14,19 +15,20 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors(
-    { 
-       origin:["http://localhost:3000"],
-       credentials: true,
-    }
-));
+app.use(cors({
+    origin: [
+        "http://localhost:3000",  // Local development URL
+        "https://fe-rfyq.onrender.com"  // Allow the frontend from Render
+    ],
+    credentials: true,  // Allow cookies to be sent across origins
+}));
 
 app.use(bodyParser.json());
 
-//Place Order
-const payment=require('./api/PaymentManagement/Payment.api.js');
-app.use('/payment',payment);
+// Payment Management
+const payment = require('./api/PaymentManagement/Payment.api.js');
+app.use('/payment', payment);
 
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     console.log('Payment Management Service is running');
 });
